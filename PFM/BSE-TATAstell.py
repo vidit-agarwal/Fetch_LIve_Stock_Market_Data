d16f0b870f57a63398ab2b1186af50a87e98b2ec
Vidit <vidit-agarwal@users.noreply.github.com>
2017-12-28 22:15:10 +0530

    Uploading PFM scripts for fetching data

---
+++ PFM/BSE-TATAstell.py
@@ -0,0 +1,81 @@
+from selenium import  webdriver
+import time
+import pymysql
+
+conn_detail = pymysql.connect(host ='pfmdb.cetl3uqzdf3o.us-east-2.rds.amazonaws.com' , unix_socket='/tmp/mysql.sock', user='root', passwd='root-pfm-password' , db='mysql' )
+
+cur2 = conn_detail.cursor()
+
+cur2.execute("USE pfmdatabase")
+
+
+driver = webdriver.PhantomJS(executable_path=r"/home/vidit/Desktop/phantomjs-2.1.1-linux-x86_64/bin/phantomjs")
+
+url_2 = "http://www.moneycontrol.com/india/stockpricequote/steel-large/tatasteel/TIS"
+
+
+while True :
+
+    #time.sleep(20)
+    driver.get(url_2)
+
+    try :
+
+        stock_exchange_bse =driver.find_element_by_xpath(".//*[@class='FR PR3']/span").text
+        print("Stock Exchange :" + stock_exchange_bse)
+
+        share_name_bse = driver.find_element_by_xpath(".//*[@class='b_42 PT5 PR']/h1").text
+        print("Share Name :" + share_name_bse)
+
+        share_rep =driver.find_element_by_xpath(".//*[@class='PB10']/div").text
+        #print(share_rep)
+
+        a,b ,c,d , e= share_rep.split("|" , 4)
+        print(a)
+
+        price_bse =driver.find_element_by_xpath(".//*[@id='Bse_Prc_tick_div']/span").text
+        print(price_bse)
+
+        price_time_bse =driver.find_element_by_xpath(".//*[@class='FL gL_10 bseArr PT4 w70']/div[3]").text
+        print(price_time_bse)
+
+        price_change = driver.find_element_by_xpath(".//*[@id='b_changetext']/span").text
+        print(price_change)
+
+        percent_change_bse = driver.find_element_by_xpath(".//*[@id='b_changetext']").text
+
+        z ,percent_change_bse = percent_change_bse.split(" " , 2)
+        print(percent_change_bse)
+
+
+
+        volume_bse = driver.find_element_by_xpath(".//*[@class='FL gL_10 PL10 PR PT15']/span/span[2]").text
+        print("Volume : "+ volume_bse)
+
+        prev_close_bse = driver.find_element_by_xpath(".//*[@class='brdb PA5']/div[1]/div[2]").text
+        print("Previoud Close :" + prev_close_bse)
+
+        open_bse = driver.find_element_by_xpath(".//*[@class='brdb PA5']/div[2]/div[2]").text
+        print("Open Proce :" + open_bse)
+
+        bid_price_bse = driver.find_element_by_xpath(".//*[@class='brdb PA5']/div[3]/div[2]").text
+        print("Bid Price Quantity :" + bid_price_bse)
+
+        offer_price_bse = driver.find_element_by_xpath(".//*[@class='brdb PA5']/div[4]/div[2]").text
+        print("Offer Price :" + offer_price_bse)
+
+        volume_bse = volume_bse.replace(',' , '')
+        percent_change_bse = percent_change_bse.strip('(').strip(')').strip('%')
+
+        cur2.execute("INSERT INTO Bse (Share_Name , Share_Symbol , Share_Price , Price_Time ,Net_Change ,Percent_Change , Volume , Open_Price , Previous_Close_Price , Bid , Offer_Price) "
+                             "VALUES (%s, %s,%s , %s , %s , %s , %s , %s , %s , %s , %s) " ,
+                    (share_name_bse , a , float(price_bse) , price_time_bse ,
+                                                                                      float(price_change) , (percent_change_bse) , (volume_bse) , float(open_bse) ,
+                                                                                      float(prev_close_bse) ,
+                                                                                     bid_price_bse , (offer_price_bse)  ) )
+        cur2.connection.commit()
+
+    except Exception as e:
+        print(e)
+
+
---
+++ PFM/scrape.py
@@ -0,0 +1,108 @@
+from selenium import  webdriver
+import time
+import pymysql
+
+conn_detail = pymysql.connect(host ='pfmdb.cetl3uqzdf3o.us-east-2.rds.amazonaws.com' , unix_socket='/tmp/mysql.sock', user='root', passwd='root-pfm-password' , db='mysql' )
+
+cur = conn_detail.cursor()
+
+cur.execute("USE pfmdatabase")
+
+
+driver = webdriver.PhantomJS(executable_path=r"/home/vidit/Desktop/phantomjs-2.1.1-linux-x86_64/bin/phantomjs")
+
+url =" http://economictimes.indiatimes.com/tata-steel-ltd/stocks/companyid-12902.cms"
+driver.get(url)
+while True :
+    time.sleep(12)
+
+    try :
+
+        stock_exchange ="NSE"
+        share_symbol = "NSE: TATASTEEL"
+                        #driver.find_element_by_xpath(".//*[@id='netspidersosh']/section[5]/section[2]/section[1]/section[2]/div[1]/ul/li[1]").text
+        print("Stock Exchange :"  , stock_exchange)
+
+        share_name = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[1]/h1").text
+      #  print(stock_exchange +" DATA  OF : "+ share_name )
+        print("\n")
+        print("Share Name : " , share_name , type(share_name))
+
+        print("Share Symbol : " , share_symbol , type(share_symbol))
+
+        share_price = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[3]/section[1]/section[1]/div[1]").text
+        #share_price = float(share_price)
+        print("Share Price : " , share_price , type(share_price))
+
+        price_time= driver.find_element_by_xpath(".//*[@class='leftData']/ul/li/div/span[2]").text
+        print("Price Time : " ,  price_time , type(price_time))
+
+
+        net_change = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[3]/section[1]/section[1]/div[2]/p[2]/span[2]").text
+        #net_change = float(net_change)
+        print("NSE Net Change :"  ,  net_change , type(net_change))
+
+        nse_percent_change = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[3]/section[1]/section[1]/div[2]/p[2]/span[3]").text
+        nse_percent_change = nse_percent_change.strip('(').strip(')').strip('%')
+        #nse_percent_change = float(nse_percent_change)
+        #print(type(nse_percent_change))
+
+        print("NSE Percent Change :" , nse_percent_change , type(nse_percent_change))
+
+        volume = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[3]/section[1]/section[1]/div[3]/p").text
+        volume = volume.replace(',' , '')
+        #volume = int(volume)
+        print('Volume :' , volume  ,  type(volume))
+
+        open_price = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[3]/section[1]/section[1]/div[4]/p[2]").text
+        #open_price = float(open_price)
+        print('open price :' , open_price  , type(open_price))
+
+        prev_close_price = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[3]/section[1]/section[1]/div[5]/p[2]").text
+        #prev_close_price = float(prev_close_price)
+        print('Previous close Price :'  , prev_close_price , type(prev_close_price))
+
+        bid = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[3]/div[2]/div[2]/ul/li[1]/span[2]").text
+        print('Bid :' , bid , type(bid))
+
+        market_cap = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[3]/div[2]/div[2]/ul/li[2]/span[2]").text
+        #print('Market Caps :' + market_cap)
+
+        pe = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[3]/div[2]/div[2]/ul/li[3]/span[2]").text
+        #print('(P/e) :' + pe)
+
+        div_yield = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[3]/div[2]/div[2]/ul/li[5]/span[2]").text
+        #print('Dividend Yield :' + div_yield)
+
+        face_value = driver.find_element_by_xpath(".//*[@id='mainContainer']/section[2]/section[1]/section[3]/div[2]/div[3]/ul/li[2]/span[2]").text
+        #print('face value :' + face_value)
+
+
+        live = driver.find_element_by_xpath(".//*[@class='leftData']/ul/li/div/span[1]").text
+        print("status" , live)
+        #market_cap = float(market_cap)
+        #pe = float(pe)
+        #div_yield = float(div_yield)
+        #face_value = int(face_value)
+        print('Market Caps :' , market_cap , type(market_cap))
+        print('(P/e) :' , pe , type(pe))
+        print('Dividend Yield :' , div_yield , type(div_yield))
+        print('face value :' , face_value , type(face_value))
+
+        cur.execute("INSERT INTO Nse (Share_Name , Share_Symbol , Share_Price , Price_Time ,Net_Change ,Percent_Change , Volume , Open_Price , Previous_Close_Price , Bid , Market_Capital , PE_Ratio , Divident_Yeild , Face_Value , status) "
+                             "VALUES (%s,           %s,             %s,             %s,         %s,     %s,                 %s,     %s,         %s,                     %s,     %s,             %s,         %s,             %s,%s) " ,
+                    (share_name , share_symbol , (share_price) , price_time ,
+                                                                                      (net_change) , (nse_percent_change) , (volume) , (open_price) ,
+                                                                                      (prev_close_price) ,
+                                                                                     bid , (market_cap) , (pe) , (div_yield) , (face_value)  , live) )
+        cur.connection.commit()
+
+
+
+
+
+    except Exception as e :
+        print("Error : " , e)
+
+cur.close()
+conn.close()
